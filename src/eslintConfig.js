157046d6304e48d4a98eea9bf04360e1c23e38f3
Robbe <robbevaes95@gmail.com>
2025-06-11 15:14:47 +0200

    feature: enforce folder structure with eslint

---
+++ src/eslintConfig.js
@@ -0,0 +1,49 @@
+import antfu from '@antfu/eslint-config'
+import globals from 'globals'
+
+import { a11yConfig } from './a11y.js'
+import { baseConfig } from './base.js'
+import { compatConfig } from './compatLint.js'
+import { i18nConfig } from './i18n.js'
+import { perfectionistConfig } from './perfectionist.js'
+import { simpleSortConfig } from './simpleSort.js'
+import { spacingConfig } from './spacing.js'
+import { projectStructureConfig } from './structure.js'
+import { tailwindConfig } from './tailwind.js'
+import { unicornConfig } from './unicorn.js'
+import { vitestConfig } from './vitest.js'
+
+export default antfu(
+  baseConfig,
+  spacingConfig,
+  perfectionistConfig,
+  unicornConfig,
+  a11yConfig,
+  ...compatConfig,
+  simpleSortConfig,
+  vitestConfig,
+  i18nConfig,
+  tailwindConfig,
+  {
+    settings: {
+      'import/extensions': [
+        '.ts',
+      ],
+      'vue-i18n': {
+        localeDir: './src/locales/*.json',
+        messageSyntaxVersion: '^9.0.0',
+      },
+    },
+
+  },
+  {
+
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.browser,
+      },
+      sourceType: 'module',
+    },
+  },
+).prepend(projectStructureConfig)
---
+++ src/structure.js
@@ -0,0 +1,487 @@
+import {
+  createFolderStructure,
+  projectStructureParser,
+  projectStructurePlugin,
+} from 'eslint-plugin-project-structure'
+
+const API_FOLDER = {
+  name: 'api',
+  children: [
+    {
+      name: '{kebab-case}',
+      children: [
+        {
+          name: 'queries',
+          children: [
+            {
+              name: '{camelCase}.query.ts',
+            },
+          ],
+        },
+        {
+          name: 'mutations',
+          children: [
+            {
+              name: '{camelCase}.mutation.ts',
+            },
+          ],
+        },
+        {
+          name: 'services',
+          children: [
+            {
+              name: '{camelCase}.service.ts',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
+
+const ASSETS_FOLDER = {
+  name: 'assets',
+  // This allows anything in the folder
+  children: [],
+}
+
+const CLIENT_FOLDER = {
+  name: 'client',
+  children: [
+    {
+      name: '{camelCase}.gen.ts',
+    },
+    {
+      name: 'index.ts',
+    },
+  ],
+}
+
+const COMPONENT_VARIANTS_FOLDER = {
+  name: 'components-variants',
+  children: [
+    {
+      name: '{kebab-case}',
+      children: [
+        {
+          name: '{camelCase}.variant.ts',
+        },
+
+      ],
+    },
+    {
+      name: 'index.ts',
+    },
+  ],
+}
+
+const COMPONENTS_FOLDER = {
+  name: 'components',
+  children: [
+    {
+      ruleId: 'components_folder',
+    },
+    {
+      name: '{PascalCase}.vue|{camelCase}.context.ts|{camelCase}.composable.ts|{camelCase}.type.ts',
+    },
+
+  ],
+}
+
+const COMPOSABLES_FOLDER = {
+  name: 'composables',
+  children: [
+    {
+      name: '{kebab-case}',
+      children: [
+        {
+          name: '{folderName}.composable.ts',
+        },
+        {
+          name: '{folderName}.composable.test.ts|{folderName}.composable.spec.ts',
+        },
+        {
+          name: '{kebab-case}',
+          children: [
+            {
+              name: '{folderName}.composable.ts',
+            },
+            {
+              name: '{folderName}.composable.test.ts|{folderName}.composable.spec.ts',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'index.ts',
+    },
+  ],
+}
+
+const CONFIGS_FOLDER = {
+  name: 'configs',
+  children: [
+    {
+      name: '{camelCase}.config.ts',
+    },
+  ],
+}
+
+const CONSTANTS_FOLDER = {
+  name: 'constants',
+  children: [
+    {
+      name: '{camelCase}.constant.ts',
+    },
+    {
+      name: '{kebab-case}',
+      children: [
+        {
+          name: '{folderName}.constant.ts',
+        },
+      ],
+    },
+  ],
+}
+
+const ICONS_FOLDER = {
+  name: 'icons',
+  children: [
+    {
+      name: '{PascalCase}Icon.vue',
+    },
+    {
+      name: 'icons.ts',
+    },
+  ],
+}
+
+const LIBS_FOLDER = {
+  name: 'libs',
+  children: [
+    {
+      name: '{camelCase}.lib.ts',
+    },
+  ],
+}
+
+const LOCALES_FOLDER = {
+  name: 'locales',
+  children: [
+    {
+      name: '{kebab-case}.json',
+    },
+  ],
+}
+
+const MIDDLEWARES_FOLDER = {
+  name: 'middlewares',
+  children: [
+    {
+      name: '{camelCase}.middleware.ts',
+    },
+  ],
+}
+
+const MOCKS_FOLDER = {
+  name: 'mocks',
+  children: [
+    {
+      name: '{camelCase}.mock.ts',
+    },
+    {
+      name: '{kebab-case}',
+      children: [
+        {
+          name: '{camelCase}.mock.ts',
+        },
+        {
+          name: 'index.ts',
+        },
+      ],
+    },
+  ],
+}
+
+const MODELS_FOLDER = {
+  name: 'models',
+  children: [
+    {
+      name: '{kebab-case}',
+      children: [
+        {
+          name: '{camelCase}.model.ts|{camelCase}.builder.ts|{camelCase}.transformer.ts|{camelCase}.modelUtil.ts|{camelCase}.modelUtil.spec.ts|{camelCase}.modelUtil.test.ts',
+        },
+        {
+          name: `create|update|delete|index|detail|{kebab-case}`,
+          children: [
+            {
+              name: '{camelCase}.model.ts|{camelCase}.builder.ts|{camelCase}.transformer.ts|{camelCase}.modelUtil.ts|{camelCase}.modelUtil.spec.ts|{camelCase}.modelUtil.test.ts',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
+
+const PLUGINS_FOLDER = {
+  name: 'plugins',
+  children: [
+    {
+      name: '{camelCase}.plugin.ts',
+    },
+
+  ],
+}
+
+const ROUTES_FOLDER = {
+  name: 'routes',
+  children: [
+    {
+      name: 'routes.ts',
+    },
+
+  ],
+}
+
+const ROUTES_MODULE_FOLDER = {
+  name: 'routes',
+  children: [
+    {
+      name: '{camelCase}.routes.ts',
+    },
+
+  ],
+
+}
+
+const STORES_FOLDER = {
+  name: 'stores',
+  children: [
+    {
+      name: '{camelCase}.store.ts',
+    },
+
+  ],
+}
+
+const THEMES_FOLDER = {
+  name: 'themes',
+  children: [
+    {
+      name: '{camelCase}.json',
+    },
+
+  ],
+}
+
+const TYPES_FOLDER = {
+  name: 'types',
+  children: [
+    {
+      name: '{kebab-case}',
+      children: [
+        {
+          name: '{camelCase}.type.ts',
+        },
+      ],
+    },
+    {
+      name: 'global',
+      children: [
+        {
+          name: '{camelCase}.d.ts',
+        },
+      ],
+    },
+    {
+      name: '{camelCase}.type.ts',
+    },
+  ],
+}
+
+const UTILS_FOLDER = {
+  name: 'utils',
+  children: [
+    {
+      name: '{kebab-case}',
+      children: [
+        {
+          name: '{camelCase}.util.ts',
+        },
+      ],
+    },
+    {
+      name: '{camelCase}.util.ts',
+    },
+  ],
+}
+
+const VIEWS_FOLDER = {
+  name: 'views',
+  children: [
+    {
+      name: '{PascalCase}View.vue',
+    },
+    {
+      name: '{PascalCase}ViewDataProvider.vue',
+    },
+  ],
+}
+
+const DIALOGS_FOLDER = {
+  name: 'dialogs',
+  children: [
+    {
+      name: '{PascalCase}Dialog.vue',
+    },
+  ],
+}
+
+export const folderStructureConfig = createFolderStructure({
+  rules: {
+    components_folder: {
+      name: '*',
+      folderRecursionLimit: 3,
+      children: [
+        {
+          ruleId: 'components_folder',
+        },
+        {
+          name: '{PascalCase}.vue|{camelCase}.context.ts|{camelCase}.composable.ts|{camelCase}.type.ts',
+        },
+      ],
+    },
+  },
+
+  structure: [
+    {
+      name: '*',
+    },
+    {
+      name: '*',
+      children: [],
+    },
+    {
+      name: 'src',
+      children: [
+        API_FOLDER,
+        ASSETS_FOLDER,
+        CLIENT_FOLDER,
+        COMPONENT_VARIANTS_FOLDER,
+        COMPONENTS_FOLDER,
+        COMPOSABLES_FOLDER,
+        CONFIGS_FOLDER,
+        CONSTANTS_FOLDER,
+        ICONS_FOLDER,
+        LIBS_FOLDER,
+        LOCALES_FOLDER,
+        MIDDLEWARES_FOLDER,
+        MOCKS_FOLDER,
+        MODELS_FOLDER,
+        PLUGINS_FOLDER,
+        ROUTES_FOLDER,
+        STORES_FOLDER,
+        THEMES_FOLDER,
+        TYPES_FOLDER,
+        UTILS_FOLDER,
+        DIALOGS_FOLDER,
+        {
+          name: 'modules',
+          children: [
+            {
+              name: '{kebab-case}',
+              children: [
+                ROUTES_MODULE_FOLDER,
+                COMPONENTS_FOLDER,
+                COMPOSABLES_FOLDER,
+                CONSTANTS_FOLDER,
+                LOCALES_FOLDER,
+                MOCKS_FOLDER,
+                MODELS_FOLDER,
+                STORES_FOLDER,
+                TYPES_FOLDER,
+                UTILS_FOLDER,
+                VIEWS_FOLDER,
+                DIALOGS_FOLDER,
+                {
+                  name: '*',
+                  children: [],
+                },
+                {
+                  name: 'features',
+                  children: [
+                    {
+                      name: '{kebab-case}',
+                      children: [
+                        COMPONENTS_FOLDER,
+                        COMPOSABLES_FOLDER,
+                        CONSTANTS_FOLDER,
+                        LOCALES_FOLDER,
+                        MOCKS_FOLDER,
+                        MODELS_FOLDER,
+                        STORES_FOLDER,
+                        TYPES_FOLDER,
+                        UTILS_FOLDER,
+                        VIEWS_FOLDER,
+                        DIALOGS_FOLDER,
+                        {
+                          name: '*',
+                          children: [],
+                        },
+
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: '*',
+          children: [],
+        },
+        {
+          name: 'main.ts',
+        },
+        {
+          name: 'App.vue',
+        },
+
+      ],
+    },
+  ],
+})
+
+export const projectStructureConfig = {
+  files: [
+    '**',
+  ],
+  ignores: [
+    'projectStructure.cache.json',
+    '**/node_modules/**',
+    '**/public/**',
+    '**/*.{png,jpg,jpeg,gif,svg,js,webp,ico,mp4,webm,mp3,wav,flac,aac,woff2,woff,eot,ttf,html,css,scss}',
+    '**/dist/**',
+    '**/.husky/**',
+    '**/.vscode/**',
+    '**/coverage/**',
+  ],
+  languageOptions: {
+    parser: projectStructureParser,
+  },
+  plugins: {
+    'project-structure': projectStructurePlugin,
+  },
+  rules: {
+    // If you have many rules in a separate file.
+    'project-structure/folder-structure': [
+      'error',
+      folderStructureConfig,
+    ],
+  },
+  settings: {},
+}
