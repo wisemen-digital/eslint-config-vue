deddbff064d5b05395b89d4afdda60999692bcc4
Robbe <robbevaes95@gmail.com>
2025-06-11 16:22:11 +0200

    chore: moved test structure

--- src/modules/other-module/features/cool-feature/coolFeature.ts
+++
@@ -1,5 +0,0 @@
-import { SECRET } from '@/modules/secret-module/components/secret'
-
-export const COOL_FEATURE = 'cool-feature'
-
-const _test = SECRET
--- src/modules/secret-module/features/secret-feature/secretFeature.ts
+++
@@ -1,8 +0,0 @@
-import { SECRET } from '@/modules/secret-module/components/secret'
-import { SECRET_CREATE } from '@/modules/secret-module/features/create/components/secretCreate'
-import { SECRET_FEATURE_2 } from '@/modules/secret-module/features/secret-feature/secretFeature2'
-
-export const SECRET_FEATURE = 'secret-create'
-const _test2 = SECRET_FEATURE_2
-const _test = SECRET
-const _test3 = SECRET_CREATE
